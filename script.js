cf4dc22fa4a90e48801ab1904e718f89fb77f9db
yanyo <honma.ryosuke@gmail.com>
2019-05-19 00:50:56 +0900

    説明を追加

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 window.onload = function() {
-  if (!'HTMLPortalElement' in window) {
-    return alert('portalが無効です。')
-  }
+  // もし Portal が利用できるプラットフォームであれば...
+  if (!'HTMLPortalElement' in window) return alert('portalが無効です。')
 
   setTimeout(main, 500)
 }
 
 function main() {
-  // もし Portal が利用できるプラットフォームであれば...
+  // portalをページに追加
   nextPagePortal = document.createElement('portal')
   nextPagePortal.classList.add('portal')
   nextLink = document.getElementsByClassName('cur')[0].nextElementSibling
   nextPagePortal.src = nextLink.getElementsByTagName('a')[0].href
 
+  // portalとemptyAreaをwrapする、スクロール領域
   scrollArea = document.createElement('div')
   scrollArea.classList.add('scroll')
 
+  // portalの表示を下にずらすためのempty領域
   emptyArea = document.createElement('div')
   emptyArea.classList.add('empty')
 
+  // 一番下までスクロールしたらportalがappendされる
   window.onscroll = function() {
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
     if (scrollTop >= document.body.offsetHeight - window.outerHeight) {
@@ -29,10 +31,13 @@ function main() {
   }
 
   scrollArea.onscroll = function() {
-    console.log(this.scrollTop, window.innerHeight)
+    // console.log(this.scrollTop, window.innerHeight)
+
+    // 上に戻ったらportal非表示
     if (this.scrollTop <= 0) {
       this.style.display = 'none'
     }
+    // スクロールし終えたらactivate
     if (this.scrollTop >= window.innerHeight) {
       nextPagePortal.activate()
     }
